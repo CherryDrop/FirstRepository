b399fa265cc52b996a859c10160341e82a0a8fc0
mikhael dantas <mikhael-dcs@outlook.com>
2022-12-02 16:31:22 -0300

    home page cards added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import HomeInfoPanel from "@myComponents/HomeInfoPanel";
 import HomePanel from "@myComponents/HomePanel";
 
 export default function Index() {
    return (
       <div>
             <HomePanel/>
+            <HomeInfoPanel/>
       </div>
    )
 }
