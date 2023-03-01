31f2f7fe841d36aa0f270331d89415dadc7cb96f
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-01 18:16:18 -0300

    implemented handler for unknown error

--- src/lib/errorHandling/ErrorPageRedirect/index.ts
+++ src/lib/errorHandling/ErrorPageRedirect/index.ts
@@ -2,11 +2,17 @@ import { IncomingMessage, ServerResponse } from "http";
 
 export function RedirectErrorFunction(res: ServerResponse<IncomingMessage>, err:any): void {
     console.log(err);
-        const [statusCode, message] = err.message.split("::::");
-        res.statusCode = parseInt(statusCode);
-        res.writeHead(302, {
-            Location: `/?error=${message}`
-        });
+    const [statusCode, message] = err.message.split("::::");
+    if (!statusCode || !message) {
+        res.statusCode = 500;
+        res.write(JSON.stringify({ error: "Sorry something Happened in error information! "}));
         res.end();
+        return;
+    }
+    res.statusCode = parseInt(statusCode);
+    res.writeHead(302, {
+        Location: `/?error=${message}`
+    });
+    res.end();
     return;
 }
