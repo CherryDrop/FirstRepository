003345365d6a8d97e2d76ee1703c98f4b4983f7c
mikhael dantas <mikhael-dcs@outlook.com>
2023-03-01 17:57:45 -0300

    omplemented server side api calls authorized

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,17 +1,24 @@
 
 export default class Api {
 
-    static baseUrl = 'http://backend:3041';
+    static baseUrl = process.env.ECOMMERCE_API_URL + '/graphql';
     public static async get<T>(path: string): Promise<T> {
         const response = await fetch(`${this.baseUrl}${path}`);
         return response.json();
     }
     // make a graphql request
-    public static async graphql<T>(query: string, variables: any): Promise<T> {
+    public static async graphql(query: string, variables: any): Promise<{data: any}> {
+        const apiKey = process.env.ECOMMERCE_API_KEY
+
+        if (this.baseUrl === undefined || !apiKey ) {
+            throw new Error('API URL or API Key not set')
+        }
+
         const response = await fetch(`${this.baseUrl}/graphql`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
+                'authorization': `Bearer ${apiKey}`
             },
             body: JSON.stringify({
                 query,
